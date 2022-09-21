467825101b998aeb84a006d5aa379957fadc3aca
Anton <antonkgeolog@gmail.com>
2022-09-21 12:23:10 +0300

    Create new Funtcion

--- StringArray/Program.cs
+++ StringArray/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] InitArray(int countOfWords)
+{
+    string[] StringArray = new string[countOfWords];
+    for(int i = 0; i < countOfWords; i ++)
+    {
+        StringArray[i] = Console.ReadLine();
+    }
+    return StringArray;
+}
+
+int CountOfShorts (string[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i ++)
+    {
+        if(array[i].Length <= 3)
+        {
+            count ++;
+        }
+    }
+}
+
+string arrayOfShorts(string[] array, int count)
+{
+    
+}
